7f25ec9a133b1d03811f4b80d2b166230c3a8fc9
Sergio <sergio-kf@mail.ru>
2022-09-22 13:22:46 +0300

    first commit

---
+++ src/components/countersList.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+    const initialState = [
+        { id: 0, value: 0, name: "Ненужная вещь"},
+        { id: 1, value: 4, name: "Ложка"},
+        { id: 2, value: 0, name: "Вилка"},
+        { id: 3, value: 0, name: "Тарелка"},
+        { id: 4, value: 0, name: "Набор минималиста"},
+    ];
+    const [counters, setCounters] = useState(initialState);
+    const handleDelete = (id) => {
+        const newCounters = counters.filter(c => c.id !== id);
+        setCounters(newCounters)
+    };
+    const handleReset = () => {
+        setCounters(initialState)
+    };
+    const handleIncrement = (id) => {
+        counters[id].value++;
+        setCounters(counters);
+    };
+    const handleDecrement = (id) => {
+        setCounters((prevState) => {
+            prevState[id].value--
+            return prevState
+        });
+    };
+    return (
+        <>
+            {counters.map((count) => (
+                <Counter
+                    key={count.id}
+                    onDelete={handleDelete}
+                    {...count}
+                    onIncrement = {handleIncrement}
+                    onDecrement = {handleDecrement}
+                />
+            ))}
+            <button className="btn btn-primary btn-sm m-2" 
+                    onClick={handleReset}>
+                Сброс
+            </button>
+        </>
+    )
+};
+
+export default CountersList;
